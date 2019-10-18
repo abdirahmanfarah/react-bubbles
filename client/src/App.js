223d90eb0518868baaab17306477ccf40811c67d
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-10-18 10:26:52 -0700

    login page work

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblesPage from './components/BubblePage'
+import PrivateRoute from './components/PrivateRoute'
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <p><Link to ='/login'>Login</Link></p>
+      <Link to ='/login'>Login</Link>
       <Link to='/BubblesPage'>Bubbles</Link>
       <Switch>
-        <PrivateRoute path='/BubblesPage' />
-        <Route exact path="/" component={Login} />
+        <PrivateRoute path='/BubblesPage'component={BubblesPage} />
+        <Route exact path="/login" component={Login} />
         
      </Switch>
    </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,57 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { axiosWithAuth } from '../utilis/axiosWithAuth';
 import { Redirect } from 'react-router-dom';
 
-const initialState= {
-  username: '',
-  password: '',
-  email: ''
-}
 
-const Login = () => {
+
+class Login extends React.Component {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-    const [login, setlogin] = useState(initialState);
+  state= {
+    credentials: {
 
-    // handleChange = e => {
-    //   setlogin({[e.target.name]: e.target.value})
-    // }
+      username: '',
+      password: '',
+      // email: ''
+    }
+  }
 
-  const formLogin = e => {
+    handleChange = e => {
+     this.setState({
+       credentials: {
+         ...this.state.credentials,
+         [e.target.name]: e.target.value
+       }
+     })
+    }
+
+ login = e => {
     e.preventDefault();
     axiosWithAuth()
-      .post('/api/login', login)
-      .then(res => console.log(res))
+      .post('/api/login', this.state.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('/BubblesPage')
+      })
       .catch(err => console.log(err.response))
   }
-  
+  render() {
+    if (localStorage.getItem('token')) {
+      return <Redirect to='/BubblesPage' />
+    }
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
-      <form onSubmit={login}>
+      <form onSubmit={this.login}>
         <input
           type='text'
           name='username'
-          // value={username}
-          // onChange={handleChange}
+          value={this.state.credentials.username}
+          onChange={this.handleChange}
         />
         <input
           type='password'
           name='password'
-          // value={password}
-          // onChange={handleChange}
-        />
-        <input
-          type='text'
-          name='email'
-          // value={email}
-          // onChange={handleChange}
+          value={this.state.credentials.password}
+          onChange={this.handleChange}
         />
       <button>Log in</button>
       </form>
     </div>
   );
 };
+}
 
 export default Login;
